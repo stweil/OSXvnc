stweil/OSXvnc

commit f66b29943ef93b5944b166640cf7424354b73a1b

Remove \n from format string

OSXvnc-server/rfbproto.h
@@ -151,7 +151,7 @@ typedef struct {
  * decode the version string respectively.
  */
 
-#define rfbProtocolVersionFormat "RFB %03d.%03d\n"
+#define rfbProtocolVersionFormat "RFB %03d.%03d"
 // Want to allow this to be specified
 //#define rfbProtocolMajorVersion 3
 //#define rfbProtocolMinorVersion 8

OSXvnc-server/rfbserver.c
@@ -353,7 +353,7 @@ rfbClientPtr rfbNewClient(int sock) {
     cl->client_zlibAfterBuf = NULL;
     cl->client_zlibAfterBufLen = 0;
 
-    sprintf(pv,rfbProtocolVersionFormat,rfbProtocolMajorVersion, rfbProtocolMinorVersion);
+    sprintf(pv, rfbProtocolVersionFormat, rfbProtocolMajorVersion, rfbProtocolMinorVersion);
 
     if (WriteExact(cl, pv, sz_rfbProtocolVersionMsg) < 0) {
         rfbLogPerror("rfbNewClient: write");
@@ -479,7 +479,7 @@ void rfbProcessClientProtocolVersion(rfbClientPtr cl) {
     }
 
     pv[sz_rfbProtocolVersionMsg] = 0;
-    if (sscanf(pv,rfbProtocolVersionFormat,&cl->major,&cl->minor) != 2) {
+    if (sscanf(pv, rfbProtocolVersionFormat, &cl->major, &cl->minor) != 2) {
 		if (strncmp(pv,"GET",3)) // Don't log if it was a browser
 			rfbLog("rfbProcessClientProtocolVersion: not a valid RFB client");
         rfbCloseClient(cl);

commit ea4d5089a209bbeafc6bcaf802aec6d5615ffcec

Fix RFB version string
This partially reverts commit f66b29943ef93b5944b166640cf7424354b73a1b
which introduced that bug.

It was reported here: https://github.com/stweil/OSXvnc/issues/9.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

OSXvnc-server/rfbproto.h
@@ -151,10 +151,7 @@ typedef struct {
  * decode the version string respectively.
  */
 
-#define rfbProtocolVersionFormat "RFB %03d.%03d"
-// Want to allow this to be specified
-//#define rfbProtocolMajorVersion 3
-//#define rfbProtocolMinorVersion 8
+#define rfbProtocolVersionFormat "RFB %03d.%03d\n"
 
 typedef char rfbProtocolVersionMsg[13]; /* allow extra byte for null */
 