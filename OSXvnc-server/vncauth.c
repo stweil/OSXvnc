5da2daa50bfffb921e52fdf88f363ecc2d117aaa
Stefan Weil <sw@weilnetz.de>
2017-12-07 21:03:48 +0100

    Fix memory leak
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- OSXvnc-server/vncauth.c
+++ OSXvnc-server/vncauth.c
@@ -119,6 +119,7 @@ vncDecryptPasswdFromFile(char *fname)
         for (i = 0; i < 8; i++) {
             ch = getc(fp);
             if (ch == EOF) {
+                free(passwd);
                 fclose(fp);
                 return NULL;
             }
