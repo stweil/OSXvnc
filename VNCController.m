stweil/OSXvnc

commit 98e436b72d384a94bab44ad2dc001f21931c4d29

Update for OSXvnc-server and storepasswd without symbolic links
Commit d1ab70551a4d265a10514214e805edc025180542 removed the symbolic
links for the command line tools, but did not update the documention
nor fix the path for OSXvnc-server.

Fix both issues.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

VNCController.m
@@ -838,7 +838,6 @@ NSMutableArray *localIPAddresses() {
     if ((argv = [self formCommandLineForSystemServer: NO])) {
         NSDictionary *infoDictionary = [NSBundle mainBundle].infoDictionary;
 
-        NSString *executionPath = [[NSBundle mainBundle].resourcePath stringByAppendingPathComponent:@"OSXvnc-server"];
         NSString *noteStartup = [NSString stringWithFormat:@"%@\tStarting %@ %@(%@)\n", [NSDate date], [NSProcessInfo processInfo].processName, [infoDictionary valueForKey:@"CFBundleShortVersion"], [infoDictionary valueForKey:@"CFBundleVersion"]];
 
 		[self determineLogLocation];
@@ -856,7 +855,7 @@ NSMutableArray *localIPAddresses() {
         [serverOutput writeData:[@"\n\n" dataUsingEncoding: NSUTF8StringEncoding]];
 
         controller = [[NSTask alloc] init];
-        controller.launchPath = executionPath;
+        controller.launchPath = @"OSXvnc-server";
         controller.arguments = argv;
         controller.standardOutput = serverOutput;
         controller.standardError = serverOutput;

OSXvnc.html
@@ -268,8 +268,7 @@ If you want to stop OSXvnc from starting up automatically any longer you can als
 <p><b><A id=Executables>Q: Where are the storepasswd and OSXvnc-server executables?</A></b></p>
 
 <p><b>A: </b> The OSXvnc-server and storepasswd executables are
-now stored in the /Contents/MacOS directory and are linked from the root
-OSXvnc.app directory as well as the /Contents/Resources directory. This
+now stored in the /Contents/MacOS directory. This
 prevents the GUI from launching a second dock icon when they are executed.</p>
 
 <p><b><A id=Logout>Q: I've set up OSXvnc as a boot-time Startup Item but when a user logs out it breaks the server.</A></b></p>

commit 60ca32c7453f87014c7b0bccb132f462f8f5e923

Fix OSXvnc-server path for system server
The path was broken in commit 98e436b72d384a94bab44ad2dc001f21931c4d29.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

VNCController.m
@@ -1498,12 +1498,14 @@ NSMutableArray *localIPAddresses() {
 
     if (success && argv) {
         NSMutableArray *copyArgsArray = [NSMutableArray array];
+        NSString *executionPath = [[NSBundle mainBundle].bundlePath
+                                   stringByAppendingPathComponent: @"Contents/MacOS/OSXvnc-server"];
 
         // Copy Server Executable
         [copyArgsArray removeAllObjects];
         [copyArgsArray addObject:@"-R"]; // Recursive
         [copyArgsArray addObject:@"-f"]; // Force Copy (overwrite existing)
-        [copyArgsArray addObject:[[NSBundle mainBundle].resourcePath stringByAppendingPathComponent:@"OSXvnc-server"]];
+        [copyArgsArray addObject:executionPath];
         [copyArgsArray addObject:launchdResources];
 
         if (![myAuthorization executeCommand:@"/bin/cp" withArgs:copyArgsArray]) {