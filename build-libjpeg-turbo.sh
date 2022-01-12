899709d525fb05332dbf3a83f692a02f227c21a7
Stefan Weil <sw@weilnetz.de>
2022-01-12 20:43:36 +0100

    Update local build rules for libjpeg-turbo
    With latest Xcode MACOSX_DEPLOYMENT_TARGET is used
    to set the deployment target.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- build-libjpeg-turbo.sh
+++ build-libjpeg-turbo.sh
@@ -7,6 +7,9 @@ export CFLAGS="-mmacosx-version-min=10.11 -Wall"
 CMAKE_BUILD_TYPE=Release
 NUMCPUS=$(sysctl -n hw.ncpu)
 
+MACOSX_DEPLOYMENT_TARGET=10.11
+export MACOSX_DEPLOYMENT_TARGET
+
 # Get submodule libjepg-turbo.
 git submodule update --init libjpeg-turbo
 
@@ -14,13 +17,13 @@ cd libjpeg-turbo
 
 mkdir -p macosarmv8
 pushd macosarmv8
-cmake -mmacosx-version-min=10.11 .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_OSX_ARCHITECTURES=arm64
+cmake .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_OSX_ARCHITECTURES=arm64
 make -j $NUMCPUS
 popd
 
 mkdir -p macosx8664
 pushd macosx8664
-cmake -mmacosx-version-min=10.11 .. -DARMV8_BUILD=../macosarmv8 -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_OSX_ARCHITECTURES=x86_64
+cmake .. -DARMV8_BUILD=../macosarmv8 -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_OSX_ARCHITECTURES=x86_64
 make -j $NUMCPUS
 popd
 
